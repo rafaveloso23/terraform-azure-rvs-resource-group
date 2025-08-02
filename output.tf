254fc6bd4df5326696150ab8e86d701ea2b8278d
rafaveloso32 <velosorafael32@gmail.com>
2025-08-02 20:44:51 -0300

    ad

--- output.tf
+++ output.tf
@@ -5,7 +5,14 @@ output "resource_group_name" {
 output "resource_group_location" {
   value = azurerm_resource_group.rg.location
 }
+output "resource_group_name_rvsfa" {
+  value = azurerm_resource_group.rg.name
+}
 
-output "resource_group_location_teste" {
+output "resource_group_location_tes02" {
+  value = azurerm_resource_group.rg.location
+}
+
+output "resource_group_location_tes03" {
   value = azurerm_resource_group.rg.location
 }
