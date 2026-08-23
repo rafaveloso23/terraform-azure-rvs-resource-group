f49a564f9fd11f2581551fb24c68494631f46b2b
rafaveloso23 <84517344+rafaveloso23@users.noreply.github.com>
2026-08-22 21:31:56 -0300

    Remove Azure provider credentialsss
    Removed sensitive Azure provider credentials from configuration.

--- providers.tf
+++ providers.tf
@@ -1,7 +1,3 @@
 provider "azurerm" {
   features {}
-  subscription_id = "cc323661-bdfb-4e37-8224-b9f41308d182"
-  client_id       = "07c8c7c7-647f-4c2f-92f9-41326eef863a"
-  client_secret   = var.client_secret
-  tenant_id       = "0eed3ea8-f35c-4862-b14a-9809318064c7"
-}#
+}
